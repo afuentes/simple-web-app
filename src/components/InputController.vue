13d1c6dcdf77dcdbf6043cd1eb195d0e5a646a5b
alex fuentes <fuentes.alex@gmail.com>
2019-05-07 00:37:17 -0400

    updated

--- src/components/InputController.vue
+++ src/components/InputController.vue
@@ -1,14 +1,30 @@
 <template>
   <footer class="bg-grey-lighter px-2 py-2 flex items-center">
-    <div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path opacity=".45" fill="#263238" d="M9.153 11.603c.795 0 1.439-.879 1.439-1.962s-.644-1.962-1.439-1.962-1.439.879-1.439 1.962.644 1.962 1.439 1.962zm-3.204 1.362c-.026-.307-.131 5.218 6.063 5.551 6.066-.25 6.066-5.551 6.066-5.551-6.078 1.416-12.129 0-12.129 0zm11.363 1.108s-.669 1.959-5.051 1.959c-3.505 0-5.388-1.164-5.607-1.959 0 0 5.912 1.055 10.658 0zM11.804 1.011C5.609 1.011.978 6.033.978 12.228s4.826 10.761 11.021 10.761S23.02 18.423 23.02 12.228c.001-6.195-5.021-11.217-11.216-11.217zM12 21.354c-5.273 0-9.381-3.886-9.381-9.159s3.942-9.548 9.215-9.548 9.548 4.275 9.548 9.548c-.001 5.272-4.109 9.159-9.382 9.159zm3.108-9.751c.795 0 1.439-.879 1.439-1.962s-.644-1.962-1.439-1.962-1.439.879-1.439 1.962.644 1.962 1.439 1.962z"></path></svg>
-    </div>
     <div class="flex-1 mx-2">
-      <input :disabled="isDisabled"  v-model="message" class="w-full border rounded px-2 py-2" type="text" placeholder="Message #general"  v-on:keyup.enter="sendMessage" />
+      <input :disabled="isDisabled"  v-model="message" class="w-full border rounded px-2 py-2" type="text" placeholder="Type Message"  v-on:keyup.enter="sendMessage" />
     </div>
     <div>
       <button :disabled="isDisabled" v-on:click="sendMessage"  >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="#263238" fill-opacity=".45" d="M11.999 14.942c2.001 0 3.531-1.53 3.531-3.531V4.35c0-2.001-1.53-3.531-3.531-3.531S8.469 2.35 8.469 4.35v7.061c0 2.001 1.53 3.531 3.53 3.531zm6.238-3.53c0 3.531-2.942 6.002-6.237 6.002s-6.237-2.471-6.237-6.002H3.761c0 4.001 3.178 7.297 7.061 7.885v3.884h2.354v-3.884c3.884-.588 7.061-3.884 7.061-7.885h-2z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="86" height="86" viewBox="0 0 86 86">
+  <defs>
+    <filter id="Elipse_11" x="0" y="0" width="86" height="86" filterUnits="userSpaceOnUse">
+      <feOffset dy="3" input="SourceAlpha"/>
+      <feGaussianBlur stdDeviation="3" result="blur"/>
+      <feFlood flood-opacity="0.161"/>
+      <feComposite operator="in" in2="blur"/>
+      <feComposite in="SourceGraphic"/>
+    </filter>
+  </defs>
+  <g id="FAB" transform="translate(-862 -1027.385)">
+    <g transform="matrix(1, 0, 0, 1, 862, 1027.38)" filter="url(#Elipse_11)">
+      <circle id="Elipse_11-2" data-name="Elipse 11" cx="34" cy="34" r="34" transform="translate(9 6)" fill="#5c7eee"/>
+    </g>
+    <g id="round-send-24px" transform="translate(893 1055.385)">
+      <path id="Trazado_949" data-name="Trazado 949" d="M0,0H24V24H0Z" fill="none"/>
+      <path id="Trazado_950" data-name="Trazado 950" d="M3.4,20.4l17.45-7.48a1,1,0,0,0,0-1.84L3.4,3.6a.993.993,0,0,0-1.39.91L2,9.12a.994.994,0,0,0,.87.99L17,12,2.87,13.88a1.012,1.012,0,0,0-.87,1l.01,4.61A.993.993,0,0,0,3.4,20.4Z" fill="#fff"/>
+    </g>
+  </g>
+</svg>
       </button>
     </div>
   </footer>
